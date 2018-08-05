b36ae26ddff8bc0f46b29735339d41153751f08c
ShawDu <ShawDu>
2018-08-05 21:49:33 +0800

    v0.0.1 初次提交

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-alert("cache-tools");
+class FastCache{
+    constructor(){
+        this.list = {}
+    }
+    set( key , value ){
+        this.list[key] = value;
+    }
+    get( key ){
+        return this.list[key];
+    }
+}
 
-
+window.FastCache = FastCache;
 
 
